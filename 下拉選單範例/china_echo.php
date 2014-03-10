a59d263669f7c0ad01adb302ca190dbbdb691d3c
terryyamg <terryyamg@hotmail.com>
2014-03-10 16:38:20 +0800

    下拉選單

---
+++ 下拉選單範例/china_echo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>無標題文件</title>
+</head>
+
+<body>
+<?
+$s1 = $_POST["s1"];
+$s2 = $_POST["s2"];
+$s3 = $_POST["s3"];
+echo "省份: ".$s1."<br>地级市: ".$s2."<br>市、县级市、县: ".$s3;
+
+?>
+</body>
+</html>
