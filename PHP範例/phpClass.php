8fb1bcbaee5d55be263d77092ae981a1e96f6f87
terry <terryyamg@hotmail.com>
2014-03-12 09:31:48 +0800

    Update phpClass.php

--- PHP範例/phpClass.php
+++ PHP範例/phpClass.php
@@ -1,4 +1,22 @@
 <?
+//基本class範例
+class right{
+  //建立一個變數
+  public $str="測試字串";
+  
+  //建立一個方法
+  public function run(){
+      //用this 輸出變數 
+      echo $this->str;
+     
+  }
+
+}
+$number = new right;
+$number->run();
+
+
+//_xxx傳遞應用
 class test{
 	//建立一個空陣列
 	var $arr = array();
